124d0ca45960fa54fd902b5296a98c50f0952e62
PedroCardoso96 <pedrohfsc96@gmail.com>
2021-09-11 13:15:06 -0300

    footer

--- main.js
+++ main.js
@@ -21,16 +21,19 @@ for(const link of links){
 
 // Change header when page scrolling
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll(){
 
-window.addEventListener('scroll', function(){
-    if(window.scrollY >= navHeight){
-        header.classList.add('scroll')
-    }else{
-        header.classList.remove('scroll')
-    }
-});
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+    
+        if(window.scrollY >= navHeight){
+            header.classList.add('scroll')
+        }else{
+            header.classList.remove('scroll')
+        }
+}
+
+
 
 
 // Testimonials carousel (Swiper)
@@ -57,5 +60,27 @@ scrollReveal.reveal(`#home .text, #home .image,
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links`,
+#contact .text, #contact .links,
+footer .brand, footer .social`,
  {interval: 100})
+
+
+//  Back-to-top
+
+
+function backToTop(){
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if(window.scrollY >= 550){
+        backToTopButton.classList.add('show')
+    } else{
+        backToTopButton.classList.remove('show')
+    }
+}
+
+// When Scroll
+
+window.addEventListener('scroll', function(){
+    changeHeaderWhenScroll()
+    backToTop()
+})
